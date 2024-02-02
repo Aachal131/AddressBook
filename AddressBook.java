8e7c155d1501c1e00439b81e62b91957cb4b90df
Aachal131 <83298583+Aachal131@users.noreply.github.com>
2024-02-02 22:12:25 +0530

    Add files via upload

--- AddressBook.java
+++ AddressBook.java
@@ -1,80 +1,5 @@
 package com.addressmanagement;
-
-import java.util.ArrayList;
 import java.util.Scanner;
-
-class Contacts {
-    // Fields
-    public String firstName;
-    public String lastName;
-    public String address;
-    public String city;
-    public String state;
-    public String zip;
-    public String phoneNumber;
-    public String email;
-    public Contacts(String firstName, String lastName, String address, String city, String state, String zip,
-                    String phoneNumber, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-    public void displayContact() {
-        System.out.println("Name: " + firstName + " " + lastName);
-        System.out.println("Address: " + address);
-        System.out.println("City: " + city);
-        System.out.println("State: " + state);
-        System.out.println("ZIP Code: " + zip);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("Email: " + email);
-    }
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void updateContact(String address, String city, String state, String zip, String phoneNumber, String email) {
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-}
-
-class Address {
-    private ArrayList<Contacts> contactsList;
-
-    public Address() {
-        contactsList = new ArrayList<>();
-    }
-
-    public void addContact(Contacts contact) {
-        contactsList.add(contact);
-    }
-    public void displayAllContacts() {
-        for (Contacts contact : contactsList) {
-            contact.displayContact();
-        }
-    }
-    public Contacts findContactByName(String fullName) {
-        for (Contacts contact : contactsList) {
-            if (contact.getFullName().equalsIgnoreCase(fullName)) {
-                return contact;
-            }
-        }
-        return null;
-    }
-
-}
-
 public class AddressBook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -100,30 +25,13 @@ public class AddressBook {
         addressBook.addContact(newContact);
         System.out.println("\nAll Contacts in Address Book:");
         addressBook.displayAllContacts();
-        // Editing an existing contact
-        System.out.println("\nEnter the full name of the contact to edit:");
-        String fullNameToEdit = scanner.nextLine();
-        Contacts contactToEdit = addressBook.findContactByName(fullNameToEdit);
-
-        if (contactToEdit != null) {
-            System.out.println("\nEnter the updated details for the contact:");
-            System.out.print("New Address: ");
-            String newAddress = scanner.nextLine();
-            System.out.print("New City: ");
-            String newCity = scanner.nextLine();
-            System.out.print("New State: ");
-            String newState = scanner.nextLine();
-            System.out.print("New ZIP Code: ");
-            String newZip = scanner.nextLine();
-            System.out.print("New Phone Number: ");
-            String newPhoneNumber = scanner.nextLine();
-            System.out.print("New Email: ");
-            String newEmail = scanner.nextLine();
-            contactToEdit.updateContact(newAddress, newCity, newState, newZip, newPhoneNumber, newEmail);
-            System.out.println("\nAll Contacts in Address Book after Update:");
-            addressBook.displayAllContacts();
-        } else {
-            System.out.println("Contact not found.");
-        }
+        System.out.println("\nEnter the name of the contact you want to edit:");
+        System.out.print("First Name: ");
+        String editFirstName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        String editLastName = scanner.nextLine();
+        addressBook.editContact(editFirstName, editLastName);
+        System.out.println("\nAll Contacts in Address Book after editing:");
+        addressBook.displayAllContacts();
     }
 }
